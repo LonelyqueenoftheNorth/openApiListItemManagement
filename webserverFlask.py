fb240a1f93a6da09a37e0f253568e2ff18f8ef49
Miia <mm@nwt.de>
2024-04-30 15:47:33 +0200

    item_add added

--- webserverFlask.py
+++ webserverFlask.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, abort
 # initialize Flask server
 app = Flask(__name__)
 
-# create unique id for lists, entries
+# create unique ID for lists, entries
 todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
 todo_list_2_id = '3062dc25-6b80-4315-bb1d-a7c86b014c65'
 todo_list_3_id = '44b02e00-03bc-451d-8d01-0c67ea866fee'
